14d4822d02f4cee1ecda870ccfa92c98017738de
hbich aymane <hbichaymane@gmail.com>
2025-03-03 22:14:43 +0000

    fix: add alias for Tabler icons to resolve issue with app not starting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tsconfigPaths(),
-  ],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
+});
